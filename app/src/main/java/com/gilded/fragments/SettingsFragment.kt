783df6fc8c9ca2966017c31932dfb588b2b2ee26
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2026-03-10 19:58:59 +0100

    Treball a classe 10/3/2026, començant el fragment de settings/preferencies

---
+++ app/src/main/java/com/gilded/fragments/SettingsFragment.kt
@@ -0,0 +1,65 @@
+package com.gilded.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.gilded.R
+import java.util.Currency
+
+
+class SettingsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val settingsFragmentView: View = inflater.inflate(R.layout.fragment_settings, container, false)
+
+        val pickCurrencyButton: Button = settingsFragmentView.findViewById(R.id.pickCurrency)
+        val currenciesConstraintLayout: ConstraintLayout = settingsFragmentView.findViewById(R.id.currencies)
+        val currenciesLinearLayout: LinearLayout = settingsFragmentView.findViewById(R.id.currenciesLinearLayout)
+        val closeDialogImageButton: ImageButton = settingsFragmentView.findViewById(R.id.closeDialog)
+
+        val closeButton: ImageButton = settingsFragmentView.findViewById(R.id.close)
+
+        for (currency in Currency.getAvailableCurrencies()) {
+            val currencyCardView: View = inflater.inflate(R.layout.currency, null)
+            val categoryNameTextView: TextView = currencyCardView.findViewById(R.id.currency)
+
+            categoryNameTextView.text = "${currency.symbol} - ${currency.displayName}"
+
+            currenciesLinearLayout.addView(currencyCardView)
+
+            currencyCardView.setOnClickListener {
+                currenciesConstraintLayout.visibility = View.GONE
+
+
+            }
+        }
+
+        pickCurrencyButton.setOnClickListener {
+            currenciesConstraintLayout.visibility = View.VISIBLE
+        }
+
+        closeDialogImageButton.setOnClickListener {
+            currenciesConstraintLayout.visibility = View.GONE
+        }
+
+        closeButton.setOnClickListener {
+            val homeFragment = HomeFragment()
+
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.fragmentContainerView, homeFragment)
+                ?.commit()
+        }
+
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+}
