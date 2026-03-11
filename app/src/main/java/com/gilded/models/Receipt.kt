8d8e43d1bd7299a671109cfd24da1be7f71fca11
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2026-03-12 00:24:48 +0100

    Treball a casa, comen

--- app/src/main/java/com/gilded/models/Receipt.kt
+++ app/src/main/java/com/gilded/models/Receipt.kt
@@ -4,5 +4,5 @@ data class Receipt (
     var recipient: String,
     var amount: Double,
     var timestamp: Long,
-    var category: String
+    var category: String 
 )
---
+++ app/src/main/java/com/gilded/services/ReceiptService.kt
@@ -0,0 +1,15 @@
+package com.gilded.services
+
+import com.gilded.models.Receipt
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ReceiptService {
+
+    @GET("/gilded/receipts")
+    suspend fun getReceipts(): Response<List<Receipt>>
+
+    @POST
+    suspend fun postReceipt(receipt: Receipt)
+}
